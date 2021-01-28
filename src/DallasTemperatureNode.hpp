5ce6bff8ed384c62ed60d1043e1a0d2a890f5be9
James Scott Jr <skoona@gmail.com>
2021-01-28 18:13:28 -0500

    Initial Commit

---
+++ src/DallasTemperatureNode.hpp
@@ -0,0 +1,71 @@
+/**
+ * Homie Node for Dallas sensors.
+ *
+ */
+
+#pragma once
+
+#include <Homie.hpp>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+class DallasTemperatureNode : public HomieNode {
+
+public:
+  DallasTemperatureNode(const char* id, const char* name, const uint8_t pin, const int measurementInterval, bool range, uint16_t lower, uint16_t upper);
+  DallasTemperatureNode(const char* id, const char* name, const uint8_t pin, const int measurementInterval);
+  
+  uint8_t       getPin() const { return _pin; }
+  void          setMeasurementInterval(unsigned long interval) { _measurementInterval = interval; }
+  unsigned long getMeasurementInterval() const { return _measurementInterval; }
+  float         getTemperature() const { return _temperature; }
+
+protected:
+  void setup() override;
+  void loop() override;
+  
+private:
+  // Total number of Sensors
+  static const uint8_t MAX_NUM_SENSORS = 8;  
+
+  // suggested rate is 1/60Hz (1m)
+  static const int MIN_INTERVAL         = 60;  // in seconds
+  static const int MEASUREMENT_INTERVAL = 300;
+
+  const char* cCaption = "• DallasTemperature sensor:";
+  const char* cIndent  = "  ◦ ";
+
+  const char* cTemperature     = "temperature";
+  const char* cTemperatureName = "Temperature";
+  const char* cTemperatureUnit = "°F";
+
+  const char* cHomieNodeState      = "state";
+  const char* cHomieNodeStateName  = "State";
+
+  const char* cHomieNodeState_OK    = "OK";
+  const char* cHomieNodeState_Error = "Error";
+  
+  /*
+  * We'll use this variable to store a found device address
+  * Can only initialize it once as definition; 
+  * - thus MAX_NUM_SENSORS is required to allocate enough address containers
+  * - could be passed as a class params, but having more than are used is not an issue
+  */
+  DeviceAddress deviceAddress[MAX_NUM_SENSORS];  
+
+  bool _sensorFound = false;
+
+  uint8_t       _pin;
+  unsigned long _measurementInterval;
+  unsigned long _lastMeasurement;
+  int _rangeCount;
+  float _temperature = NAN;
+
+  OneWire*           oneWire;
+  DallasTemperature* sensor;
+  uint8_t            numberOfDevices;  // Number of temperature devices found
+
+  void   initializeSensors();
+  void   printCaption();
+  String address2String(const DeviceAddress deviceAddress);
+};
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+/**
+ * @file main.cpp
+ * @author James Scott, Jr.  aka(Skoona) (skoona@gmail.com)
+ * @brief HomieNode featuring an DS18B20 Temperature Class, 
+ *        using HomeNode and/or HomieRange methods.
+ * @version 1.0.0
+ * @date 2021-01-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <Arduino.h>
+#include <OneWire.h>
+#include <Homie.h>
+#include "DallasTemperatureNode.hpp"
+
+#ifdef ESP8266
+extern "C"
+{
+#include "user_interface.h" // to set CPU Freq for Non-Huzzah's
+}
+#endif
+
+/*
+ * SKN_NODE_ID becomes the base name when ranges are used
+ * ex: sknSensors/deviceName/DS18B20_0/temperature -> 72.3 degress
+ * Note: HomieNode(...range,lower,upper) manages this array suffix change; i.e no more name fixups
+*/
+#define SKN_MOD_NAME "Environment-DS18B20"
+#define SKN_MOD_VERSION "1.0.0"
+#define SKN_MOD_BRAND "SknSensors"
+#define SKN_MOD_TITLE "Environment Monitor"
+#define SKN_NODE_ID "DS18B20" // "environmentMonitor"
+
+
+
+const uint8_t DTN_RANGE_LOWER = 0;
+const uint8_t DTN_RANGE_UPPER = 3;
+const uint8_t PIN_DS_POOL = D4; // Pin of Temp-Sensor Pool
+const uint8_t TEMP_READ_INTERVALL = 30;
+
+DallasTemperatureNode environmentMonitor(SKN_NODE_ID, SKN_MOD_TITLE, PIN_DS_POOL, TEMP_READ_INTERVALL, true, DTN_RANGE_LOWER, DTN_RANGE_UPPER);
+
+bool broadcastHandler(const String &level, const String &value)
+{
+  Homie.getLogger() << "Received broadcast level " << level << ": " << value << endl;
+  return true;
+}
+
+void setup() {
+  delay(50);
+  Serial.begin(115200);
+  Serial << endl
+         << endl;
+
+  Homie_setFirmware(SKN_MOD_NAME, SKN_MOD_VERSION);
+  Homie_setBrand(SKN_MOD_BRAND);
+
+  Homie.setBroadcastHandler(broadcastHandler)
+       .setup();
+}
+
+void loop() {
+  Homie.loop();
+}
